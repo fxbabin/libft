7f9699ae3e9d2e587cd027a05c4e2e98a9a1df5a
Francois-xavier BABIN <fbabin@e3r4p12.42.fr>
2017-12-20 20:52:44 +0100

    corrected strspn

--- libft/ft_strcspn.c
+++ libft/ft_strcspn.c
@@ -6,27 +6,18 @@
 /*   By: fbabin <fbabin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/24 13:37:54 by fbabin            #+#    #+#             */
-/*   Updated: 2017/12/02 21:45:00 by fbabin           ###   ########.fr       */
+/*   Updated: 2017/12/20 20:49:31 by fbabin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strcspn(const char *s, const char *charset)
+size_t			ft_strcspn(const char *s, const char *charset)
 {
-	size_t		i;
-	int			y;
+	int		i;
 
-	i = 0;
-	while (s[i])
-	{
-		y = -1;
-		while (charset[++y])
-		{
-			if (s[i] == charset[y])
-				return (i);
-		}
-		i++;
-	}
+	i = -1;
+	while (s[++i] && !ft_charinset(s[i], charset))
+		;
 	return (i);
 }
