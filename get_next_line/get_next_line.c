79ac0a6e278a636ee2e55dc26f41bec351b75459
Francois-xavier BABIN <fbabin@e3r13p10.42.fr>
2018-01-17 16:11:18 +0100

    corrected gnl

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,15 +6,75 @@
 /*   By: fbabin <fbabin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/18 14:04:29 by fbabin            #+#    #+#             */
-/*   Updated: 2018/01/16 23:08:45 by fbabin           ###   ########.fr       */
+/*   Updated: 2018/01/17 15:38:28 by fbabin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int				get_next_line(const int fd, char **line)
+void			ft_gnlremovefd(t_gnl **begin_gnl, int fd)
 {
-	static char		buff[BUFF_SIZE + 1];
+	t_gnl	*tmp;
+	t_gnl	*curr;
+
+	while (*begin_gnl && (*begin_gnl)->fd == fd)
+	{
+		tmp = *begin_gnl;
+		*begin_gnl = (*begin_gnl)->next;
+		free(tmp);
+	}
+	curr = *begin_gnl;
+	while (curr && curr->next)
+	{
+		if (curr->next->fd == fd)
+		{
+			tmp = curr->next;
+			curr->next = tmp->next;
+			free(tmp);
+		}
+		if (curr->next)
+			curr = curr->next;
+	}
+}
+
+void			ft_gnlpushfront(t_gnl **begin_gnl, int fd)
+{
+	t_gnl		*t;
+
+	if (!begin_gnl)
+		return ;
+	if ((t = (t_gnl*)malloc(sizeof(t_gnl))) == NULL)
+		return ;
+	(t)->fd = fd;
+	(t)->next = NULL;
+	ft_bzero((t)->buff, BUFF_SIZE + 1);
+	if (*begin_gnl)
+	{
+		t->next = (*begin_gnl);
+		(*begin_gnl) = t;
+	}
+	else
+		(*begin_gnl) = t;
+}
+
+t_gnl			*ft_gnlfindfd(t_gnl *begin_list, int fd)
+{
+	t_gnl		*tmp;
+
+	if (!begin_list)
+		return (begin_list);
+	tmp = begin_list;
+	while (tmp)
+	{
+		if (tmp->fd == fd)
+			return (tmp);
+		tmp = tmp->next;
+	}
+	return (tmp);
+}
+
+int				inner_get_next_line(const int fd, char **line, char *buff)
+{
 	int				ret;
 	int				endl;
 	int				idx;
@@ -39,4 +99,22 @@ int				get_next_line(const int fd, char **line)
 		if (endl)
 			return (1);
 	}
+}
+
+int				get_next_line(const int fd, char **line)
+{
+	static t_gnl		*g;
+	t_gnl				*tmp;
+	int					ret;
+
+	if (fd < 0)
+		return (-1);
+	if (!(tmp = ft_gnlfindfd(g, fd)))
+	{
+		ft_gnlpushfront(&g, fd);
+		tmp = ft_gnlfindfd(g, fd);
+	}
+	if ((ret = inner_get_next_line(fd, line, tmp->buff)) == 0)
+		ft_gnlremovefd(&g, fd);
+	return (ret);
 }
