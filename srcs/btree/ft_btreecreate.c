52fe4b75cb14d89450dd87f62825dc1b0c5a2e52
Francois-xavier BABIN <fbabin@e3r12p18.42.fr>
2018-04-16 01:40:47 +0200

    changed libft structure

--- srcs/btree/ft_btreecreate.c
+++ srcs/btree/ft_btreecreate.c
@@ -1,33 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   btree_create_node.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fbabin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/08/15 16:16:51 by fbabin            #+#    #+#             */
-/*   Updated: 2018/04/16 01:31:35 by fbabin           ###   ########.fr       */
+/*   Created: 2016/08/24 15:25:19 by fbabin            #+#    #+#             */
+/*   Updated: 2018/04/16 01:02:17 by fbabin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-
-/*
-** --------------------------------- INCLUDES -------------------------------
-*/
-
-# include <unistd.h>
-# include <string.h>
-# include <stdlib.h>
-
-#include "array.h"
 #include "btree.h"
-#include "convert.h"
-#include "io.h"
-#include "lst.h"
 #include "mem.h"
-#include "str.h"
-#include "utils.h"
 
-#endif
+t_btree		*ft_btreecreate(void *item)
+{
+	t_btree		*bt;
+
+	if (!(bt = (t_btree*)ft_memalloc(sizeof(t_btree))))
+		return (NULL);
+	bt->item = item;
+	bt->left = NULL;
+	bt->right = NULL;
+	return (bt);
+}
