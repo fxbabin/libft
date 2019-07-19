add77c3fbf13ddae03ef68385fc250f1d88347ea
Francois-xavier BABIN <fbabin@e1r12p16.42.fr>
2019-07-19 15:46:08 +0200

    quicksort is now fully modular

--- includes/array.h
+++ includes/array.h
@@ -6,7 +6,7 @@
 /*   By: fbabin <fbabin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/16 00:50:15 by fbabin            #+#    #+#             */
-/*   Updated: 2018/04/16 00:56:14 by fbabin           ###   ########.fr       */
+/*   Updated: 2019/07/19 15:43:01 by fbabin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,12 @@
 ** --------------------------- ARRAY FUNCTIONS ----------------------------
 */
 
+int		            ft_qs_intcmp(void *n1, void *n2);
+int		            ft_qs_strcmp(void *s1, void *s2);
+
 char				*ft_join(char **tab, char *sep);
-void				ft_quicksort(int **array, int left, int right);
-void				ft_quicksort2(int **array, int left, int right);
-void				ft_strquicksort(char **array, int left, int right);
+void			    ft_quicksort(void **array, int left, int right,
+                        int (cmp)(void*, void*));
 void				ft_mergesort(int **array, int left, int right);
 void				ft_strmergesort(char **array, int left, int right);
 void				ft_char2dump(char **array);
--- srcs/array/ft_quicksort.c
+++ srcs/array/ft_quicksort.c
@@ -6,27 +6,60 @@
 /*   By: fbabin <fbabin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/20 22:15:35 by fbabin            #+#    #+#             */
-/*   Updated: 2018/04/16 00:39:36 by fbabin           ###   ########.fr       */
+/*   Updated: 2019/07/19 15:41:17 by fbabin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "array.h"
-#include "utils.h"
 
-static int		ft_partition(int **array, int left, int right)
+int		ft_qs_intcmp(void* n1, void *n2)
 {
 	int		pivot;
 
-	pivot = *array[(left + right) / 2];
+	pivot = *((int*)n2);
+	return (*((int*)n1) - pivot);
+}
+
+int		ft_qs_strcmp(void *s1, void *s2)
+{
+	const unsigned char		*tmp1;
+	const unsigned char		*tmp2;
+
+	tmp1 = (const unsigned char *)s1;
+	tmp2 = (const unsigned char *)s2;
+	while (*tmp1 || *tmp2)
+	{
+		if (*tmp1 != *tmp2)
+			return (*tmp1 - *tmp2);
+		tmp1++;
+		tmp2++;
+	}
+	return (0);
+}
+
+void	ft_qs_swap(void **arr, int left, int right)
+{
+	void	*tmp;
+
+	tmp = arr[left];
+	arr[left] = arr[right];
+	arr[right] = tmp;
+}
+
+static int		ft_partition(void **array, int left, int right, int (cmp)(void*, void*))
+{
+	void	*pivot;
+
+	pivot = array[(left + right) / 2];
 	while (left <= right)
 	{
-		while (*array[left] < pivot)
+		while (cmp(array[left], pivot) < 0)
 			left++;
-		while (*array[right] > pivot)
+		while (cmp(array[right], pivot) > 0)
 			right--;
 		if (left <= right)
 		{
-			ft_swap(array[left], array[right]);
+			ft_qs_swap(array, left, right);
 			left++;
 			right--;
 		}
@@ -34,13 +67,13 @@ static int		ft_partition(int **array, int left, int right)
 	return (left);
 }
 
-void			ft_quicksort(int **array, int left, int right)
+void			ft_quicksort(void **array, int left, int right, int (cmp)(void*, void*))
 {
 	int		index;
 
 	if (left >= right || left < 0 || right < 0 || !array)
 		return ;
-	index = ft_partition(array, left, right);
-	ft_quicksort(array, left, index - 1);
-	ft_quicksort(array, index, right);
+	index = ft_partition(array, left, right, cmp);
+	ft_quicksort(array, left, index - 1, cmp);
+	ft_quicksort(array, index, right, cmp);
 }
