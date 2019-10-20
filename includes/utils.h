b58b387567a7639168b6a1e8d8f118cddd5680a2
Francois-xavier BABIN <fbabin@e3r8p16.42.fr>
2019-10-20 21:55:51 +0200

    feat : added getopt functions

--- includes/utils.h
+++ includes/utils.h
@@ -6,7 +6,7 @@
 /*   By: fbabin <fbabin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/16 01:28:40 by fbabin            #+#    #+#             */
-/*   Updated: 2018/04/16 01:29:39 by fbabin           ###   ########.fr       */
+/*   Updated: 2019/10/20 21:51:11 by fbabin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,13 @@
 /*
 ** ----------------------------- UTILS FUNCTIONS ------------------------------
 */
+
+typedef struct			s_optlist
+{
+	char				flag;
+	char				*param;
+	struct s_optlist	*next;
+}						t_optlist;
 
 int					ft_isalnum(int c);
 int					ft_iswp(char c);
@@ -29,6 +36,12 @@ int					ft_tolower(int c);
 int					ft_abs(int nb);
 void				ft_swap(int *a, int *b);
 int					ft_tabsize(void **tab);
-char				*ft_getopt(int argc, char **argv, const char *opts);
+
+void				ft_optlist_pushback(t_optlist **begin_list,
+						char flag, char *value);
+void				*ft_optlist_del(t_optlist **begin);
+void				display_optlist(t_optlist *opt);
+int					ft_getopt(t_optlist **opts, int argc,
+						char **argv, char *avail_opts);
 
 #endif
